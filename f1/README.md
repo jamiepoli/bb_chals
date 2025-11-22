8a4f0d49c00b2a6e3dfc249895c81132adaee767
Vie <vie@Vies-MacBook-Pro-2.local>
2025-11-21 17:48:52 -0800

    f1

---
+++ f1/README.md
@@ -0,0 +1,21 @@
+# FORMULA ONE
+
+SCUDERIA FERRARI ES MI RELIGIÓN
+
+This is a beginner-friendly challenge meant to encourage players to get familiar with client-side APIs. There will be many that will pop up in a client-side challenge, and in this case, players ideally will learn about a few very common ones through this challenge.
+
+No hints will be given unless no one solves this challenge, in which a public hint may be provided. 
+
+If you find the flag, feel free to DM it to me and I can verify. 
+
+Let's treat this like a legit CTF challenge. I'd rather you not share the flag or share hints outside of what has already been discussed publicly. 
+
+This challenge should be solvable if you're willing to apply yourself. It's not solvable in a few minutes (I think?) but you won't need a month to solve it either. 
+
+After some time I can talk about this chal and maybe even provide a writeup once I stop procrastinating on writing my PlaidCTF and GoogleCTF author writeups.
+
+To run this challenge:
+
+```
+docker build . -t formula && docker run -p 8990:8990 -t formula
+```
---
+++ f1/src/index.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import serialize from 'serialize-javascript';
+import visit from './visit';
+
+const app = express();
+const PORT = process.env.port || 8090;
+
+app.use(express.json());
+
+
+function recreate(new_body, body) {
+    for (var key in body) {
+        if (typeof body[key] == 'object') {
+            var replacement = body[key];
+            key = key.replaceAll("<", "_").replaceAll(">", "_");
+            recreate(new_body[key], replacement);
+        } else {
+            new_body[key] = body[key];
+        }
+    }
+    return new_body;
+}
+var sanitize_keys = (req, res, next) => {
+    var new_body = recreate({}, req.body);
+    req.body = new_body;
+    return next();
+};
+
+app.get('/', (req, res) => {
+    if (req.query) {
+        return res.status(200).send(serialize(req.query));
+    } else return res.status(200).send("hi!");
+    
+});
+
+var clean_url = (req, res, next) => {
+    if (req.body.url) {
+        if (req.body.url.startsWith("http://") || req.body.url.startsWith("https://")) {
+            return next();
+        }
+    } return res.redirect('/'); 
+}
+
+app.post('/report', clean_url, (req, res) => {
+    if (req.body.url) {
+        visit(req.body.url);
+    }
+    return res.status(200).send('URL sent.'); 
+});
+
+app.post('/', sanitize_keys, (req, res) => {
+    return res.redirect('/');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
